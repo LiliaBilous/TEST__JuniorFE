8c19b782ce8022c9b31dbeb9fbe858702ea33d46
Liliia Bilous <liliyabilous.email@gmail.com>
2024-04-15 18:43:27 +0300

    worked with addaptive header

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-let menuBtn = document.querySelector(".menu__icon");
-let menu = document.querySelector(".menu");
+let menuBtn = document.querySelector(".menu__burger-icon");
+let menu = document.querySelector(".menu__body");
 
 menuBtn.addEventListener("click", function () {
-  menuBtn.classList.toggle("menu-open");
+  menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
 });
 
