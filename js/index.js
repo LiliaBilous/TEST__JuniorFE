0271df5f1437873ebf0f5bd82e391fb247c9ca81
Liliia Bilous <liliyabilous.email@gmail.com>
2024-04-17 13:31:41 +0300

    added smooth scrolling

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Header burger menu
 let menuBtn = document.querySelector(".menu__burger-icon");
 let menu = document.querySelector(".menu__body");
 
@@ -6,6 +7,7 @@ menuBtn.addEventListener("click", function () {
   menu.classList.toggle("active");
 });
 
+// Hero swiper
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
@@ -20,4 +22,19 @@ const swiper = new Swiper(".swiper", {
     el: ".slider__fraction",
     type: "fraction",
   },
+});
+
+// Scrolling to Anchor Smoothly
+document.querySelectorAll(".menu__link").forEach((el) => {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    const headerHeight = document.querySelector(".header").offsetHeight;
+
+    window.scrollTo({
+      top: target.offsetTop - headerHeight,
+      behavior: "smooth",
+    });
+  });
 });
