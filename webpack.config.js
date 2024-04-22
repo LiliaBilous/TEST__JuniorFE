ec8d89ca113ca63af9f90b37906c69b2ffab860f
Liliia Bilous <liliyabilous.email@gmail.com>
2024-04-22 20:17:11 +0300

    customized webpack config

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    filename: "[name][contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    open: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs",
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Test Task Digital Progect by LiliiaBilous",
+      filename: "index.html",
+      template: "./src/template.html",
+    }),
+  ],
+};
